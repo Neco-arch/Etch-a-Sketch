0ed4e56370fe131f0b2898cdb66551710a829edf
Neco-arch <111620463+Neco-arch@users.noreply.github.com>
2025-03-02 13:16:30 +0700

    Finish my project

--- Script/script.js
+++ Script/script.js
@@ -1,20 +1,76 @@
 const Drawing_Grid = document.querySelector(".Drawing-Grid")
-const Clear_Button = document.querySelector(".Clear_Button")
+const Eraser_Button = document.querySelector(".Eraser_Button")
 const Change_Size_Button = document.querySelector(".Change_Size_Button")
+const Drawable_Grid = document.querySelectorAll(".Div_Drawing")
+const Drawing_Button = document.querySelector(".Drawing_Button")
+const ColorPicker = document.querySelector(".Color_Picker")
+let mode = "Drawing"
 
 
+function StarterGrid() {
+    let Grid = String((700 / 16));
+    let height = String((700 / 16));
+    Drawing_Grid.innerHTML = "";
 
-function UserChoose() {
-    let NumberGird = prompt("Number of squares per side:")
-    if (typeof(NumberGird)!== Number) {
-        return
-    } else {
-        return NumberGird
-    }
-
+    for (let i = 1; i <= 16 ; i++ ) {
+        let div = document.createElement("div");
+        div.className = "Drawing_Row";
+        div.style.width = Grid + "px";
+        Drawing_Grid.appendChild(div);
+        for (let b = 1; b <= 16 ; b++) {
+            let Div_Drawing  = document.createElement("div");
+            Div_Drawing.className = "Div_Drawing"
+            Div_Drawing.style.width = Grid + "px";
+            Div_Drawing.style.height = height + "px"
+            div.appendChild(Div_Drawing);
+        };
+    };
 };
 
+function CreateGrid() {
+    let User_Squares = prompt("How many square you want on each sides");
+    let Grid = String((700 / User_Squares));
+    let height = String((700 / User_Squares));
+    Drawing_Grid.innerHTML = "";
+
+    for (let i = 1; i <= User_Squares ; i++ ) {
+        let div = document.createElement("div");
+        div.className = "Drawing_Row";
+        div.style.width = Grid + "px";
+        Drawing_Grid.appendChild(div);
+        for (let b = 1; b <= User_Squares ; b++) {
+            let Div_Drawing  = document.createElement("div");
+            Div_Drawing.className = "Div_Drawing"
+            Div_Drawing.style.width = Grid + "px";
+            Div_Drawing.style.height = height + "px"
+            div.appendChild(Div_Drawing);
+        };
+    };
+};
+
+Drawing_Grid.addEventListener('mouseover', (event) =>{
+    let targetSquare =  event.target;
+    switch (mode) {
+        case "Drawing":
+            targetSquare.style.backgroundColor = ColorPicker.value;
+            break;
+    
+        case "Erase":
+            targetSquare.style.backgroundColor = "white";
+            break;
+    }
+})
+
+function ToggleEraser (){
+    mode = "Erase"
+};
+
+function ToggleDrawing_mode () {
+    mode = "Drawing"
+}
 
 
-
-Change_Size_Button.addEventListener("click", () => CreateGrid())
+Change_Size_Button.addEventListener("click", () => CreateGrid());
+document.addEventListener("DOMContentLoaded", () => StarterGrid());
+Eraser_Button.addEventListener("click", () => ToggleEraser())
+Drawing_Button.addEventListener("click", () => ToggleDrawing_mode())
